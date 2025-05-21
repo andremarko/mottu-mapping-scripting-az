b335925ee2f33b974991d2d61cde77c829c5dbf8
andremarko <151890840+andremarko@users.noreply.github.com>
2025-05-21 15:49:12 -0300

    chore: renomeia scripts em ordem

---
+++ 02-vm-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+sudo dnf update -y
+sudo dnf upgrade -y
+sudo dnf install -y dnf-plugins-core
+
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install -y docker-ce docker-ce-cli containerd.io
+
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo groupadd docker || true
+sudo usermod -aG docker $(whoami)
+
+sudo dnf install -y unzip
+sudo dnf install -y java-21-openjdk-devel
+
+wget https://download.oracle.com/otn_software/java/sqldeveloper/sqlcl-latest.zip
+unzip sqlcl-latest.zip
+rm -f sqlcl-latest.zip
+sudo mv sqlcl /opt/sqlcl
+sudo ln -s /opt/sqlcl/bin/sql /usr/local/bin/sql
+
+echo "Usuário adicionado ao grupo docker. Por favor, reconecte-se para que as permissões tenham efeito."
+exit
---
+++ 03-vm-deploy-backend-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+git clone https://github.com/andremarko/mottu-mapping-api-java.git
+
+cd mottu-mapping-api-java
+
+docker build --build-arg DB_USER=rm555285 --build-arg DB_PASSWORD=270102 -t ${projectName}-backend-image .
+
+docker run -d --name "${projectName}-backend" -p 8080:8080 "${projectName}-backend-image"
