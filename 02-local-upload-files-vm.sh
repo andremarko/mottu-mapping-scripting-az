4582b50b7ee0f0e68373d3c253436b03602e982b
andremarko <151890840+andremarko@users.noreply.github.com>
2025-05-21 16:56:50 -0300

    feat(upload-files): adiciona scp copia de chave ssh para maquina virtual para autenticar clone do repositorio do githhub.

--- 02-local-upload-files-vm.sh
+++ 02-local-upload-files-vm.sh
@@ -7,4 +7,6 @@ VM_IP=$(az vm show \
   --query publicIps \
   --output tsv)
 
+scp -i $SSH_KEY ~/.ssh/id_ed25519.pub $USERNAME@$VM_IP:~/.ssh/
 scp -i $SSH_KEY [0-9][0-9]-vm* $USERNAME@$VM_IP:/home/$USERNAME/
+
