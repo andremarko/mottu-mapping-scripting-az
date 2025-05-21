51452140b66f824247cecf8904ce74288128a467
andremarko <151890840+andremarko@users.noreply.github.com>
2025-05-21 19:49:24 -0300

    alteracoes

--- 03-vm-setup.sh
+++ 03-vm-setup.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -e
 
-cat ~/.ssh/id_ed25519.pub >> ~/.ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
-rm ~/.ssh/id_ed25519.pub
-
-sudo dnf update -y
-sudo dnf upgrade -y
-sudo dnf install -y dnf-plugins-core
-
 sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo dnf install -y docker-ce docker-ce-cli containerd.io
 
@@ -25,11 +17,11 @@ ssh -o StrictHostKeyChecking=no -T git@github.com
 sudo dnf install -y unzip
 sudo dnf install -y java-21-openjdk-devel
  
-wget https://download.oracle.com/otn_software/java/sqldeveloper/sqlcl-latest.zip
-unzip sqlcl-latest.zip
-rm -f sqlcl-latest.zip
-sudo mv sqlcl /opt/sqlcl
-sudo ln -s /opt/sqlcl/bin/sql /usr/local/bin/sql
+wget https://download.oracle.com/otn_software/java/sqldeveloper/sqlcl-latest.zip \
+unzip sqlcl-latest.zip \
+rm -f sqlcl-latest.zip \
+sudo mv sqlcl /opt/sqlcl \
+sudo ln -s /opt/sqlcl/bin/sql /usr/local/bin/sql 
 
 echo "Usuário adicionado ao grupo docker. Por favor, reconecte-se para que as permissões tenham efeito."
 exit
