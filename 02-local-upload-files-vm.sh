d68681f956192593a66592df771ebd5b6b9e433b
andremarko <151890840+andremarko@users.noreply.github.com>
2025-05-21 18:52:32 -0300

    correcao caracteres

--- 02-local-upload-files-vm.sh
+++ 02-local-upload-files-vm.sh
@@ -7,6 +7,8 @@ VM_IP=$(az vm show \
   --query publicIps \
   --output tsv)
 
-scp -i $SSH_KEY ~/.ssh/id_ed25519.pub $USERNAME@$VM_IP:~/.ssh/
-scp -i $SSH_KEY [0-9][0-9]-vm* $USERNAME@$VM_IP:/home/$USERNAME/
+VM_IP=$(echo "$VM_IP" | tr -d '\r\n')
+USERNAME=$(echo "$USERNAME" | tr -d '\r\n')
 
+scp -v -i "$SSH_KEY" "$HOME/.ssh/id_ed25519.pub" "$USERNAME@$VM_IP:/home/$USERNAME/.ssh/"
+scp -v -i "$SSH_KEY" [0-9][0-9]-vm* "$USERNAME@$VM_IP:/home/$USERNAME/"
