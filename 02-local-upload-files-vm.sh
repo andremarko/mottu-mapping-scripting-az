00e5ea9e3411bb8c17637ebba29b1dd494d57c1e
andremarko <151890840+andremarko@users.noreply.github.com>
2025-05-21 19:17:03 -0300

    alteracoes caminho ssh_key

--- 02-local-upload-files-vm.sh
+++ 02-local-upload-files-vm.sh
@@ -10,5 +10,5 @@ VM_IP=$(az vm show \
 VM_IP=$(echo "$VM_IP" | tr -d '\r\n')
 USERNAME=$(echo "$USERNAME" | tr -d '\r\n')
 
-scp -v -i "$SSH_KEY" "$HOME/.ssh/id_ed25519.pub" "$USERNAME@$VM_IP:/home/$USERNAME/.ssh/"
-scp -v -i "$SSH_KEY" [0-9][0-9]-vm* "$USERNAME@$VM_IP:/home/$USERNAME/"
+scp -v -i $SSH_KEY "$HOME/.ssh/id_ed25519.pub" "$USERNAME@$VM_IP:/home/$USERNAME/.ssh/"
+scp -v -i $SSH_KEY [0-9][0-9]-vm* "$USERNAME@$VM_IP:/home/$USERNAME/"
