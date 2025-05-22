6595b082a285146c1ac9bc3b9676e9877281e777
andremarko <151890840+andremarko@users.noreply.github.com>
2025-05-22 14:14:24 -0300

    remove instalacao de sqlcli e java

--- 03-vm-setup.sh
+++ 03-vm-setup.sh
@@ -12,16 +12,5 @@ sudo usermod -aG docker $(whoami)
 sudo dnf install -y git
 git config --global user.name "andremarko"
 git config --global user.email "andre.marcolongo@gmail.com"
-ssh -o StrictHostKeyChecking=no -T git@github.com
+ssh -o StrictHostKeyChecking=no -T git@github.com 
 
-sudo dnf install -y unzip
-sudo dnf install -y java-21-openjdk-devel
- 
-wget https://download.oracle.com/otn_software/java/sqldeveloper/sqlcl-latest.zip \
-unzip sqlcl-latest.zip \
-rm -f sqlcl-latest.zip \
-sudo mv sqlcl /opt/sqlcl \
-sudo ln -s /opt/sqlcl/bin/sql /usr/local/bin/sql 
-
-echo "Usuário adicionado ao grupo docker. Por favor, reconecte-se para que as permissões tenham efeito."
-exit
